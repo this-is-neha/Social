93013a2a1b9b7e3d38a56d41141a9fd1d0cb627f
this-is-neha <nehushah12@gmail.com>
2025-05-24 15:22:06 +0545

    Nehaaaaddf

---
+++ clie/src/main.tsx
@@ -0,0 +1,28 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import Routing from './routing.config.js';
+import { Provider } from "react-redux";
+import {store} from './redux/store.js';
+import { UserProvider } from './constext/following.context.js';
+import { FollowerProvider } from './constext/followers.context.js';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <Provider store={store}>
+        <UserProvider>
+          <FollowerProvider>
+            <BrowserRouter>
+              <Routing />
+            </BrowserRouter>
+          </FollowerProvider>
+        </UserProvider>
+      </Provider>
+    </StrictMode>,
+  );
+} else {
+  throw new Error("Root element with id 'root' not found");
+}
