a046c171f300231e435d4eaec41c36490686e8c2
this-is-neha <nehushah12@gmail.com>
2025-05-24 19:28:32 +0545

    Nehaaniceuull

--- clie/src/main.tsx
+++ clie/src/main.tsx
@@ -1,3 +1,33 @@
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import './index.css';
+// import { BrowserRouter } from 'react-router-dom';
+// import Routing from './routing.config.js';
+// import { Provider } from "react-redux";
+// import { store } from './redux/store.js';
+
+// import { UserProvider } from './constext/following.context.js';
+// import { FollowerProvider } from './constext/followers.context.js';
+
+// const rootElement = document.getElementById('root');
+// if (!rootElement) {
+//   throw new Error("Root element with id 'root' not found");
+// }
+
+// createRoot(rootElement).render(
+//   <StrictMode>
+    
+// <Provider store={store}>
+//     <UserProvider>
+//       <FollowerProvider>
+//         <BrowserRouter>
+//           <Routing />
+//         </BrowserRouter>
+//       </FollowerProvider>
+//     </UserProvider>
+//     </Provider>
+//   </StrictMode>,
+// );
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
@@ -16,15 +46,14 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    
-<Provider store={store}>
-    <UserProvider>
-      <FollowerProvider>
-        <BrowserRouter>
-          <Routing />
-        </BrowserRouter>
-      </FollowerProvider>
-    </UserProvider>
+    <Provider store={store}>
+      <UserProvider>
+        <FollowerProvider>
+          <BrowserRouter>
+            <Routing />
+          </BrowserRouter>
+        </FollowerProvider>
+      </UserProvider>
     </Provider>
   </StrictMode>,
 );
